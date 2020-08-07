ccb977ea689f4a00ac5d10bbf2d86a6408b83ecb
Anshul <anshulwagadre@gmail.com>
2020-08-08 00:12:52 +0530

    general module imports

--- js/app.js
+++ js/app.js
@@ -1,1 +1,26 @@
-console.log('Loaded');
+import { createUser, postProcessUsers } from './users.module.js';
+
+let inputs = {
+  users: [
+    {
+      name: 'Cersei Lannister',
+      birthday: '11/30/1975',
+    },
+    {
+      name: 'Tyrion Lannister',
+      birthday: '12/02/1978',
+    },
+    {
+      name: 'Jon Snow',
+      birthday: '12/03/1989',
+    },
+  ],
+  yearInput: '1980',
+};
+
+let users = inputs.users.map((curr) => {
+  return createUser(curr, inputs.yearInput);
+});
+users = postProcessUsers(users);
+
+console.log('users', users);
