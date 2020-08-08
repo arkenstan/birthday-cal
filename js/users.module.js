0aa49947258b1d4f63feb2d1578a368022863afa
Anshul <anshulwagadre@gmail.com>
2020-08-08 15:48:29 +0530

    refactored user module for user data processing

--- js/users.module.js
+++ js/users.module.js
@@ -1,55 +1,62 @@
-/**
- *
- * @param {object} userInput
- * @param {string} userInput.name
- * @param {string} userInput.birthday
- * @param {string} inputYear
- *
- * @returns {object}
- */
+import { globalData } from './helpers.js';
 
-function createUser(userInput, inputYear) {
-  const { name, birthday } = userInput;
-  let userDetails = {
-    name,
-    dob: birthday,
-    initials: null,
-    birthDay: null,
-    age: null,
-    isValid: true,
-  };
-
-  // Create Date details
-  const dob = new Date(birthday);
-  const dobCurrentYear = new Date(birthday);
-  dobCurrentYear.setFullYear(inputYear);
-  userDetails.age = dobCurrentYear.getTime() - dob.getTime();
-
-  if (userDetails.age < 0) {
-    userDetails.isValid = false;
+function processInitials(userName, initials) {
+  let nameSplitted = userName.split(' ');
+  let spaceRemoved = userName.replace(' ', '');
+  let inital = '';
+  if (nameSplitted.length > 1) {
+    inital = `${nameSplitted[0][0]}${nameSplitted[nameSplitted.length - 1][0]}`;
+  } else {
+    inital = `${spaceRemoved.charAt(0)}${spaceRemoved.charAt(1)}`;
   }
-
-  if (userDetails.isValid) {
-    // Get Day of birth in current year
-    userDetails.birthDay = dobCurrentYear.getDay();
-
-    // Create Initials Details
-    const nameSplit = name.split(' ');
-    userDetails.initials = nameSplit.reduce((acc, curr) => {
-      acc += curr[0];
-      return acc;
-    }, '');
+  let i = 1;
+  while (initials.includes(inital) && i <= spaceRemoved.length) {
+    inital = `${spaceRemoved.charAt(0)}${spaceRemoved.charAt(i)}`;
   }
-
-  return userDetails;
+  return inital.toUpperCase();
 }
 
-function postProcessUsers(users) {
-  users = users.filter((el) => el.isValid);
-  users = users.sort((el1, el2) => {
-    return el1.age - el2.age;
+function processDate(dob, year) {
+  let actualDob = new Date(dob);
+  let currentDob = new Date(dob);
+  currentDob.setFullYear(year);
+  const dayOfWeek = currentDob.getDay();
+  const age = currentDob.getTime() - actualDob.getTime();
+  return { dob, currentDob, age, dayOfWeek };
+}
+
+function processUserInput(users, year) {
+  let initialsArray = [];
+  const { days } = globalData;
+  let processedUsers = [];
+  for (let user of users) {
+    const { name, birthday } = user;
+    let temp = {
+      ...user,
+      currentDob: null,
+      initials: null,
+      ageTime: null,
+      dayOfWeek: null,
+    };
+    let { currentDob, age, dayOfWeek } = processDate(birthday, year);
+    if (age >= 0) {
+      const initials = processInitials(name, initialsArray);
+      console.log('processUserInput -> initals', initials);
+      initialsArray.push(initials);
+      temp = {
+        ...temp,
+        initials,
+        currentDob,
+        ageTime: age,
+        dayOfWeek: days[dayOfWeek],
+      };
+      processedUsers.push(temp);
+    }
+  }
+  processedUsers = processedUsers.sort((el1, el2) => {
+    return el1.ageTime - el2.ageTime;
   });
-  return users;
+  return processedUsers;
 }
 
-export { createUser, postProcessUsers };
+export { processUserInput };
