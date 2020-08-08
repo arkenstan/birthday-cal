dc17d0d746349b49f2458326f7769f34717b4012
Anshul <anshulwagadre@gmail.com>
2020-08-08 11:30:28 +0530

    Adjusted layout printing to shift sunday to the back

--- js/layout.module.js
+++ js/layout.module.js
@@ -1,9 +1,15 @@
-import { days } from './helpers.js';
+import { globalData } from './helpers.js';
 
 function createCalCards() {
-  let cardsContainers = document.getElementById('cardsContainer');
+  const { days } = globalData;
+  let printDays = [...days];
+  printDays.shift();
+  printDays.push(days[0]);
 
-  for (let day of days) {
+  let cardsContainers = document.getElementById('cardsContainer');
+  let cards = [];
+
+  for (let day of printDays) {
     let card = document.createElement('div');
     card.setAttribute('class', 'card');
 
@@ -18,13 +24,38 @@ function createCalCards() {
     // Create card content
     let cardContent = document.createElement('div');
     cardContent.setAttribute('class', 'card-content');
-    cardContent.setAttribute('id', `card-${day}`);
+    cardContent.setAttribute('id', `${globalData.cardIdPrefix}${day}`);
     cardContent.innerHTML = `<div class="none"><h1>None</h1></div>`;
 
-    card.appendChild(cardHeader);
-    card.appendChild(cardContent);
-    cardsContainers.append(card);
+    card.append(cardHeader, cardContent);
+    cards.push(card);
+  }
+
+  cardsContainers.append(...cards);
+}
+
+function addUsersToCards(users) {
+  let dayContainers = {};
+
+  for (let user of users) {
+    if (!dayContainers[user.birthDay]) {
+      dayContainers[user.birthDay] = [];
+    }
+    let userCard = document.createElement('div');
+    userCard.setAttribute('class', 'user');
+    userCard.innerText = user.initials;
+    dayContainers[user.birthDay].push(userCard);
+  }
+
+  for (let day of globalData.days) {
+    let contentEl = document.getElementById(`${globalData.cardIdPrefix}${day}`);
+    contentEl.innerHTML = ``;
+    if (dayContainers[day] && dayContainers[day].length > 0) {
+      contentEl.append(...dayContainers[day]);
+    } else {
+      contentEl.innerHTML = `<div class="user none-color"><h1>None</h1></div>`;
+    }
   }
 }
 
-export { createCalCards };
+export { createCalCards, addUsersToCards };
