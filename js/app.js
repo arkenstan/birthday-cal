c3de6959a53c5a581a437db8a59ba54e8819a60f
Anshul <anshulwagadre@gmail.com>
2020-08-08 11:29:36 +0530

    moved sample inputs to helper and refactored to a initialize function

--- js/app.js
+++ js/app.js
@@ -1,33 +1,15 @@
+import { sampleInputs } from './helpers.js';
 import { createUser, postProcessUsers } from './users.module.js';
-import { createCalCards } from './layout.module.js';
+import { createCalCards, addUsersToCards } from './layout.module.js';
 
-function initializeUI() {
+function initialize() {
   createCalCards();
+  let users = sampleInputs.users.map((curr) => {
+    return createUser(curr, sampleInputs.yearInput);
+  });
+  users = postProcessUsers(users);
+  console.table(users);
+  addUsersToCards(users);
 }
 
-window.onload = initializeUI();
-
-let inputs = {
-  users: [
-    {
-      name: 'Cersei Lannister',
-      birthday: '11/30/1975',
-    },
-    {
-      name: 'Tyrion Lannister',
-      birthday: '12/02/1978',
-    },
-    {
-      name: 'Jon Snow',
-      birthday: '12/03/1989',
-    },
-  ],
-  yearInput: '1980',
-};
-
-let users = inputs.users.map((curr) => {
-  return createUser(curr, inputs.yearInput);
-});
-users = postProcessUsers(users);
-
-console.log('users', users);
+window.onload = initialize;
