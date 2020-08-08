a6a4bc1592460504bbdaacf7d0b6bcb954fde3bc
Anshul <anshulwagadre@gmail.com>
2020-08-08 15:49:49 +0530

    scaffolding refactoring

--- js/app.js
+++ js/app.js
@@ -1,13 +1,28 @@
 import { sampleInputs } from './helpers.js';
-import { createUser, postProcessUsers } from './users.module.js';
+import { processUserInput } from './users.module.js';
 import { createCalCards, addUsersToCards } from './layout.module.js';
+import { validateInputs } from './validations.js';
+
+function initializeForms() {
+  let inputForm = document.forms['birthdayForm'];
+  inputForm.addEventListener('submit', (ev) => {
+    ev.preventDefault();
+    let formData = validateInputs(inputForm);
+    console.log('initializeForms -> formData', formData);
+    if (formData.errors.length > 0) {
+    } else {
+      const { birthdays, year } = formData;
+      let users = processUserInput(birthdays, year);
+      console.log('initializeForms -> users', users);
+    }
+  });
+}
 
 function initialize() {
   createCalCards();
-  let users = sampleInputs.users.map((curr) => {
-    return createUser(curr, sampleInputs.yearInput);
-  });
-  users = postProcessUsers(users);
+  initializeForms();
+  let users = processUserInput(sampleInputs.users, sampleInputs.yearInput);
+  // console.log('initialize -> users', users);
   console.table(users);
   addUsersToCards(users);
 }
